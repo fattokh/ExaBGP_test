8d1f07931690898e7a4b8909d9da558b00810955
Fattokh Sadriddinov <fattokh.s@gmail.com>
2023-06-09 14:30:16 +0200

    final

--- run.bash
+++ run.bash
@@ -13,8 +13,9 @@ cd /home/user/scaling-eureka/tb/exa
 wget https://lxd.s3.eu-south-1.amazonaws.com/exa.tar.gz
 wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
 git clone https://github.com/fattokh/ExaBGP_test.git
+cd /home/user/scaling-eureka/tb/exa/ExaBGP_test
 cp exa.tar.gz ../
 cp frr.tar.gz ../
-
+cd ../
 lxc list
 python3 exa.py EXA
