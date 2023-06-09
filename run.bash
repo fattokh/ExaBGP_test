e7935fa8cea54dc08b320352c4e5b1022d278053
Fattokh Sadriddinov <fattokh.s@gmail.com>
2023-06-09 14:23:55 +0200

    final

--- run.bash
+++ run.bash
@@ -9,7 +9,16 @@ wget https://drive.google.com/uc?export=download&id=1uBTLPXLNLcZrMhgudTlUkfBMXi1
 wget https://drive.google.com/uc?export=download&id=1EV8lSoZ8Hz91ytNhw5b_THxMrFXawYBk
 wget https://drive.google.com/uc?export=download&id=1rBM-ta1liYfbevTPguq-ySXTnJfv6Q68
 sudo apt install net-tools
-python3 bgp4.py TB
-lxc list
+python3 bgp.py TB
+
 pip install exabgp
 sudo apt-get install debootstrap
+cd /home/user/scaling-eureka/tb/exa
+wget https://lxd.s3.eu-south-1.amazonaws.com/exa.tar.gz
+wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
+git clone https://github.com/fattokh/ExaBGP_test.git
+cp exa.tar.gz ../
+cp frr.tar.gz ../
+
+lxc list
+python3 exa.py EXA
