9a44a4d33c77be59c590c33cb81531d37c008751
Fattokh Sadriddinov <fattokh.s@gmail.com>
2023-06-09 14:44:08 +0200

    final

--- run.bash
+++ run.bash
@@ -10,10 +10,10 @@ python3 bgp.py TB
 pip install exabgp
 sudo apt-get install debootstrap
 cd /home/user/scaling-eureka/tb/exa
-wget https://lxd.s3.eu-south-1.amazonaws.com/exa.tar.gz
 wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
 git clone https://github.com/fattokh/ExaBGP_test.git
 cd /home/user/scaling-eureka/tb/exa/ExaBGP_test
+tar -czvf exa.tar.gz metadata.yaml 
 cp exa.tar.gz ../
 cp frr.tar.gz ../
 cd ../
