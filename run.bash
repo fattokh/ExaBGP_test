8d736c9c1e0ba313fcabac14e747ea9d6783da59
Fattokh Sadriddinov <fattokh.s@gmail.com>
2023-06-20 18:15:30 +0200

    wdw

--- run.bash
+++ run.bash
@@ -4,33 +4,42 @@ cd /home/user/
 mkdir exa_test
 mkdir frr_test
 git clone https://github.com/falberti/scaling-eureka.git
-cd /home/user/scaling-eureka/tb/bgp/
+
+cd /home/user/scaling-eureka/
+
+#rm exa.py
+wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
+wget https://lxd.s3.eu-south-1.amazonaws.com/exa.tar.gz
+cd 
+cp /home/user/ExaBGP_test/exa_metadata.tar.gz  /home/user/scaling-eureka/tb/exa
+cp /home/user/ExaBGP_test/frr_metadata.tar.gz  /home/user/scaling-eureka/tb/exa
+cp exa.tar.gz           /home/user/scaling-eureka/tb/exa
+cp frr.tar.gz           /home/user/scaling-eureka/tb/exa
+cp /home/user/ExaBGP_test/exa_metadata.tar.gz  /home/user/scaling-eureka/tb/bgp
+cp /home/user/ExaBGP_test/frr_metadata.tar.gz  /home/user/scaling-eureka/tb/bgp
+cp exa.tar.gz           /home/user/scaling-eureka/tb/bgp
+cp frr.tar.gz           /home/user/scaling-eureka/tb/bgp
+cp /home/user/ExaBGP_test/exa_metadata.tar.gz  /home/user/scaling-eureka/tb/bgp4
+cp /home/user/ExaBGP_test/frr_metadata.tar.gz  /home/user/scaling-eureka/tb/bgp4
+cp exa.tar.gz           /home/user/scaling-eureka/tb/bgp4
+cp frr.tar.gz           /home/user/scaling-eureka/tb/bgp4
+cd /home/user/scaling-eureka/tb/exa
+
+
+cd /home/user/scaling-eureka/tb/bgp4/
 wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
 sudo apt install net-tools
-python3 bgp.py TB
+python3 bgp4.py TB
 pip install exabgp
 sudo apt-get install debootstrap
 
-cd /home/user/scaling-eureka/
-./create_image_exa.sh /home/user/exa_test
-cp exa_metadata.tar.gz  /home/user/scaling-eureka/tb/exa
-cp frr_metadata.tar.gz  /home/user/scaling-eureka/tb/exa
-cp exa.tar.gz           /home/user/scaling-eureka/tb/exa
-cp frr.tar.gz           /home/user/scaling-eureka/tb/exa
-
-cd /home/user/scaling-eureka/tb/exa
-#rm exa.py
-wget https://lxd.s3.eu-south-1.amazonaws.com/frr.tar.gz
-#wget https://lxd.s3.eu-south-1.amazonaws.com/exa.tar.gz
-
-
-git clone https://github.com/fattokh/ExaBGP_test.git
-cd /home/user/scaling-eureka/tb/exa/ExaBGP_test
+#git clone https://github.com/fattokh/ExaBGP_test.git
+#cd /home/user/scaling-eureka/tb/exa/ExaBGP_test
 #tar -czvf exa_metadata.tar.gz metadata.yaml 
 #cp exa_metadata.tar.gz ../
-cp frr_metadata.tar.gz ../
-cp exa.py ../
-cd ../
+#cp frr_metadata.tar.gz ../
+#cp exa.py ../
+#cd ../
 lxc list
-python3 exa.py EXA 
-lxc list
+#python3 exa.py EXA 
+#lxc list
