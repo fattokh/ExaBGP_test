091e7ea520419f55c0de200585aca2e0c4e75f76
Fattokh Sadriddinov <fattokh.s@gmail.com>
2023-07-02 22:41:03 +0200

    f

--- run.bash
+++ run.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd /home/user/ExaBGP_test
-#Fattokh
-#Fict4ia
-
-
 curl -L "https://www.dropbox.com/scl/fi/86b0njiz79wz6f46505po/exa.tar.gz?dl=0&rlkey=33hvy6x7n1dci4op5qlvhoid6" > exa.tar.gz
 curl -L "https://www.dropbox.com/scl/fi/r0j7b6ih7h1fheczp5a3n/frr.tar.gz?dl=0&rlkey=330ix39vt2ccbsovsgqzb70ix" > frr.tar.gz
 
@@ -11,5 +7,6 @@ sudo apt install net-tools
 pip install exabgp
 sudo apt-get install debootstrap
 cd /home/user/ExaBGP_test
-
+python3 exa.py TB
+lxc list
 
