19ddc2836b4a59678aeb1ce99ae52e64f8166c7c
Fattokh Sadriddinov <fattokh.s@gmail.com>
2023-07-04 15:52:31 +0200

    final

--- run.bash
+++ run.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/user/ExaBGP_test
-curl -L "https://www.dropbox.com/scl/fi/86b0njiz79wz6f46505po/exa.tar.gz?dl=0&rlkey=33hvy6x7n1dci4op5qlvhoid6" > exa.tar.gz
-curl -L "https://www.dropbox.com/scl/fi/r0j7b6ih7h1fheczp5a3n/frr.tar.gz?dl=0&rlkey=330ix39vt2ccbsovsgqzb70ix" > frr.tar.gz
 
+curl -L "https://www.dropbox.com/scl/fi/r0j7b6ih7h1fheczp5a3n/frr.tar.gz?dl=0&rlkey=330ix39vt2ccbsovsgqzb70ix" > frr.tar.gz
+curl -L "https://www.dropbox.com/scl/fi/xse6kx12bpoxx14m1ikls/exa.tar.gz?rlkey=1xsyta0l9dnxrep7pzre1cl7l" > exa.tar.gz
 sudo apt install net-tools
 pip install exabgp
 sudo apt-get install debootstrap
